d75c7bb6096388aa04bde1766d69007c8ae89d6a
Jeff L <jeff@rubbingalcoholic.com>
2014-11-05 18:13:59 -0800

    initial checkin

---
+++ _includes/share_tweet_text.js
@@ -0,0 +1,6 @@
+//
+//  WARNING! ~ If you need to use an apostrophe ==>'<==, precede it with a backslash
+//  So instead of "Bill O'Reilly" you would write "Bill O\'Reilly".
+//  If you fail to do this, everything will be ruined.
+//
+TWEET_TEXT = 'Obama can help save Net Neutrality or destroy it — along with his legacy. Here\'s how we\'ll force his hand: www.didobamabreakthenet.com'
---
+++ js/main.js
@@ -0,0 +1,108 @@
+// MT1.11 Compat
+var $E = function(selector, filter){
+	return ($(filter) || document).getElement(selector);
+};
+
+var $ES = function(selector, filter){
+	return ($(filter) || document).getElements(selector);
+};
+
+var trackOptimizely = function(ev) {
+    window['optimizely'] = window['optimizely'] || [];
+    window.optimizely.push(["trackEvent", ev]);
+};
+
+jQuery( document ).ready(function( $ ) {
+
+    var validatePhone = function(num) {
+        num = num.replace(/\s/g, '').replace(/\(/g, '').replace(/\)/g, '');
+        num = num.replace("+", "").replace(/\-/g, '');
+
+        if (num.charAt(0) == "1")
+            num = num.substr(1);
+
+        if (num.length != 10)
+            return false;
+
+        return num;
+    }
+
+    $('#phoneForm').submit(function(e) {
+        e.preventDefault();
+        $('#call_button').click();
+    });
+
+    $('#call_button').click(function(e) {
+
+        var phone = $('#phone').val();
+
+        if (!validatePhone(phone))
+            return alert('Please enter a valid US phone number!');
+
+        var data = {
+            campaignId: 'whitehouse-blanket', 
+            userPhone: validatePhone(phone),
+            zipcode: '95051'
+        }
+
+        $.ajax({
+            url: 'https://call-congress.fightforthefuture.org/create',
+            type: "get",
+            dataType: "json",
+            data: data,
+            success: function(res) {
+                trackOptimizely('call_fcc');
+                console.log('Placed call-congress call: ', res);
+            }
+        });
+        $('.overlay').css('display', 'table');
+        setTimeout(function() {
+            $('.overlay').addClass('visible');
+            setTimeout(function() {
+                $('.overlay .modal .inner').addClass('visible');
+            }, 10);
+        }, 100);
+    });
+
+    $('#emailForm').submit(function(e) {
+        e.preventDefault();
+        $('#email_button').click();
+    });
+
+    $('#email_button').click(function(e) {
+
+        if (!validateEmail($('#email').val()))
+            return alert('Please enter a valid email address!');
+
+        $('#email_form_fields').addClass('fade');
+        $('.thanks').addClass('visible');
+        setTimeout(function() {
+            $('#email_form_fields').hide();
+        }, 500);
+
+        var form = $('#emailForm');
+        $.post(form.attr('action'), form.serialize(), function(data){});
+
+    });
+
+    $('a.twitter').click(function(e) {
+
+        e.preventDefault();
+
+        trackOptimizely('share');
+
+        var tw_text = encodeURIComponent(TWEET_TEXT);
+        window.open('https://twitter.com/intent/tweet?hashtags=&text='+ tw_text +'&related=fightfortheftr');
+
+    });
+
+    $('.a.facebook').click(function(e) {
+        trackOptimizely('share');
+    });
+
+});
+
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
